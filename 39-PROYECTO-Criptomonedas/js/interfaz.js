5b913d0999482516f151213a160f1301a19c9431
robertojimenez2 <83432109+robertojimenez2@users.noreply.github.com>
2021-07-06 14:54:25 -0500

    Delete 39-PROYECTO-Criptomonedas directory

--- 39-PROYECTO-Criptomonedas/js/interfaz.js
+++
@@ -1,9 +0,0 @@
-export const criptomonedasSelect = document.querySelector('#criptomonedas')
-export const monedaSelect = document.querySelector('#moneda')
-export const form = document.querySelector('#formulario')
-export const resultado = document.querySelector('#resultado')
-
-export const objBusqueda = {
-    moneda: '',
-    criptomoneda: ''
-}
